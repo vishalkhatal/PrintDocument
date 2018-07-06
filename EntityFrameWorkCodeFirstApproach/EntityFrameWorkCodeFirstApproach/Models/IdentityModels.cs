cc3ee7e91b280fed770040e20e8159b4a9f323db
Vishal Khatal <vishal.khatal2206@gmail.com>
2018-07-06 11:53:22 +0530

    Added basic skeleton

---
+++ EntityFrameWorkCodeFirstApproach/EntityFrameWorkCodeFirstApproach/Models/IdentityModels.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using EntityFrameWorkCodeFirstApproach.Migrations;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace EntityFrameWorkCodeFirstApproach.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+        public DbSet<Department> Departments { get; set; }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<ApplicationDbContext, Configuration>());
+        }
+    }
+}
---
+++ EntityFrameWorkCodeFirstApproach/EntityFrameWorkCodeFirstApproach/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EntityFrameWorkCodeFirstApproach.Startup))]
+namespace EntityFrameWorkCodeFirstApproach
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
