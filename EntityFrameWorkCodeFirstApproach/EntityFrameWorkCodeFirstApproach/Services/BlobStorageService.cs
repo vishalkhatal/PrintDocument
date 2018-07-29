5fd0625703c4e3d5a8a98e66106dcb86718100ba
Vishal Khatal <vishal.khatal2206@gmail.com>
2018-07-29 17:43:51 +0530

    removed secret

--- EntityFrameWorkCodeFirstApproach/EntityFrameWorkCodeFirstApproach/Services/BlobStorageService.cs
+++ EntityFrameWorkCodeFirstApproach/EntityFrameWorkCodeFirstApproach/Services/BlobStorageService.cs
@@ -13,7 +13,7 @@ namespace EntityFrameWorkCodeFirstApproach.Services
         public CloudBlobContainer GetCloudBlobContainer()
         {
             // Retrieve storage account from connection string.
-            CloudStorageAccount storageAccount = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=vishalgit;AccountKey=S808gjNhyH2gNMmpiXZjdp8FBCS2X2hSV+VeEUFuO/m9TIgrrc/YtLQhSiDJ/9Yfat7HGgRvUopaCKoyMLhALQ==;EndpointSuffix=core.windows.net");
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse("");
 
             // Create the blob client.
             CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
